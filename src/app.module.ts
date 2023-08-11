35fa4970086dd7ea505c9ca554be04c29d936e03
yongseok kim <yongseok@saerom.co.kr>
2023-08-11 10:53:06 +0900

    2.3 TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,24 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { RestaurantModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'test',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+      entities: [],
     }),
     RestaurantModule,
   ],
